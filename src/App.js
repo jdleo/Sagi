43bb3ddffcedcd9303602c253ffa0868ba9f622d
John Leonardo <jdleonardo5@icloud.com>
2018-05-28 23:18:39 -0700

    basic UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, DataTable, TableHeader, Cell, Grid} from 'react-mdl';
 
 class App extends Component {
+    
+  constructor(props){
+        super(props);
+        this.state = {
+            legitScore: 0,
+            scamScore: 0
+        }
+      
+        this.onPress = this.onPress.bind(this);
+  }
+    
+  onPress() {
+      this.setState({
+            legitScore: 55,
+            scamScore: 55
+        })
+  }
+    
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+            <Grid shadow={0}>
+            <Cell col={12}>
+                <Button raised accent ripple onClick={this.onPress}>
+                    Analyze
+                </Button>
+            </Cell>
+            <Cell col={12}>
+                <DataTable style={{width: '80%', margin: 'auto'}}
+                    shadow={0}
+                    rows={[
+                        {class: 'Legit', score: this.state.legitScore},
+                        {class: 'Scam', score: this.state.scamScore}
+                    ]}
+                >
+                    <TableHeader name="class" tooltip="Classification, scam or legit">Class</TableHeader>
+                    <TableHeader numeric name="score" tooltip="Euclidean Distance Average against these datasets">Score</TableHeader>
+                </DataTable>
+            </Cell>
+            </Grid>
+        </div>
     );
   }
 }
